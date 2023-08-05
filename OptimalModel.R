d6cdb22bd31ab46b1fe21bfd271197ac3f48db2c
ILIKEWIND123 <141470742+ILIKEWIND123@users.noreply.github.com>
2023-08-05 11:58:16 +0800

    Add files via upload

---
+++ OptimalModel.R
@@ -0,0 +1,68 @@
+# This code already contains the training set data, the user only needs to enter the test data
+
+# Note that to be consistent with the denoised reflectance in our paper
+
+# Please use wheat canopy reflectance data from the grain filling stage
+
+#In your test data, the first column should be the measured yield, column name Y, and the subsequent columns should be the reflectance of the bands at different wavelengths. The column names are the wavelengths
+
+testdata<- read.table("clipboard",header=TRUE,sep='')#Input test data
+
+BagRS_MLR<-function(traindata,testdata,sample_rate_col,cycletime){
+  
+  set.seed(10)
+  
+  pre_matrix<-matrix(ncol=cycletime,nrow=nrow(testdata))
+  
+  for (i in 1:cycletime) {
+    
+    sample_index_col<-sample(2:ncol(traindata),floor(sample_rate_col*ncol(traindata)))
+    
+    sample_index_col<-append(1,sample_index_col)
+    
+    sample_index_row<-sample(1:nrow(traindata), replace = TRUE)
+    
+    sample_data<-traindata[sample_index_row,sample_index_col]
+    
+    model<-lm(Y~.,data=sample_data)
+    
+    sample_data_te<-testdata[,sample_index_col]
+    
+    pre_matrix[,i]<-predict(model,newdata=sample_data_te)
+    
+  }
+  
+  outpre_matrix<-cbind(testdata$Y,pre_matrix)
+  
+  means<-rowMeans(pre_matrix)
+  
+  out<-c()
+  
+  pre_matrix[pre_matrix < 0] <- 0
+  
+  meanp<-matrix(ncol=1,nrow=nrow(testdata))
+  
+  for (j in 1:nrow(testdata)) {
+    
+    inid<-which(pre_matrix[j,]==0,arr.ind=T)
+    
+    if(length(inid)>0){
+      
+      meanp[j,1]<-median(pre_matrix[j,-inid])
+      
+    }else{
+      
+      meanp[j,1]<-median(pre_matrix[j,])
+      
+    }
+    
+  }
+  
+  results<-list(final_pre=meanp,pre_matrix=pre_matrix,outpre_matrix=outpre_matrix)
+  
+  return(results)
+}#Constructing a function for BagRS-MLR model
+
+PRE<-BagRS_MLR(traindata=traindata,testdata=testdata,sample_rate_col=0.005,cycletime=500)$final_pre#Predictions on the test set
+
+sample_index_row<-sample(1:nrow(traindata), replace = TRUE)
